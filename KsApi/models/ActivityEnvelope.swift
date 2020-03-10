659f06feeb8ccb80c7eb644ace59a3e22d30b198
Jan Timar <jantimar2@gmail.com>
2020-03-10 17:25:32 +0100

    Extend ActivityEnvelope with Swift.Decodable

--- KsApi/models/ActivityEnvelope.swift
+++ KsApi/models/ActivityEnvelope.swift
@@ -2,11 +2,11 @@ import Argo
 import Curry
 import Runes
 
-public struct ActivityEnvelope {
+public struct ActivityEnvelope: Swift.Decodable {
   public let activities: [Activity]
   public let urls: UrlsEnvelope
 
-  public struct UrlsEnvelope {
+  public struct UrlsEnvelope: Swift.Decodable {
     public let api: ApiEnvelope
 
     public struct ApiEnvelope {
@@ -34,5 +34,18 @@ extension ActivityEnvelope.UrlsEnvelope.ApiEnvelope: Argo.Decodable {
   public static func decode(_ json: JSON) -> Decoded<ActivityEnvelope.UrlsEnvelope.ApiEnvelope> {
     return curry(ActivityEnvelope.UrlsEnvelope.ApiEnvelope.init)
       <^> (json <| "more_activities" <|> .success(""))
+  }
+}
+
+extension ActivityEnvelope.UrlsEnvelope.ApiEnvelope: Swift.Decodable {
+  private enum CodingKeys: String, CodingKey {
+    case moreActivities = "more_activities"
+  }
+
+  public init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    moreActivities = container.decodeOptional(.moreActivities)
+      ?? ""
   }
 }
