90fcb46aa71f07e0bf7210c919f46628cf1f4557
Jan Timar <jantimar2@gmail.com>
2020-03-11 14:55:05 +0100

    Remove Argo dependency from Update

--- KsApi/models/Update.swift
+++ KsApi/models/Update.swift
@@ -1,7 +1,4 @@
-import Argo
-import Curry
 import Foundation
-import Runes
 
 public struct Update {
   public let body: String?
@@ -18,10 +15,10 @@ public struct Update {
   public let user: User?
   public let visible: Bool?
 
-  public struct UrlsEnvelope {
+  public struct UrlsEnvelope: Decodable {
     public let web: WebEnvelope
 
-    public struct WebEnvelope {
+    public struct WebEnvelope: Decodable {
       public let update: String
     }
   }
@@ -33,45 +30,7 @@ public func == (lhs: Update, rhs: Update) -> Bool {
   return lhs.id == rhs.id
 }
 
-extension Update: Argo.Decodable {
-  public static func decode(_ json: JSON) -> Decoded<Update> {
-    let tmp1 = curry(Update.init)
-      <^> json <|? "body"
-      <*> json <|? "comments_count"
-      <*> json <|? "has_liked"
-    let tmp2 = tmp1
-      <*> json <| "id"
-      <*> json <| "public"
-      <*> json <|? "likes_count"
-    let tmp3 = tmp2
-      <*> json <| "project_id"
-      <*> json <|? "published_at"
-      <*> json <| "sequence"
-      <*> (json <| "title" <|> .success(""))
-    return tmp3
-      <*> json <| "urls"
-      <*> json <|? "user"
-      <*> json <|? "visible"
-  }
-}
-
-extension Update.UrlsEnvelope: Argo.Decodable {
-  public static func decode(_ json: JSON) -> Decoded<Update.UrlsEnvelope> {
-    return curry(Update.UrlsEnvelope.init)
-      <^> json <| "web"
-  }
-}
-
-extension Update.UrlsEnvelope.WebEnvelope: Argo.Decodable {
-  public static func decode(_ json: JSON) -> Decoded<Update.UrlsEnvelope.WebEnvelope> {
-    return curry(Update.UrlsEnvelope.WebEnvelope.init)
-      <^> json <| "update"
-  }
-}
-
-// MARK: - Swift decodable
-
-extension Update: Swift.Decodable {
+extension Update: Decodable {
   private enum CodingKeys: String, CodingKey {
     case body, id, sequence, title, urls, user, visible
     case commentsCount = "comments_count"
@@ -82,7 +41,3 @@ extension Update: Swift.Decodable {
     case publishedAt = "published_at"
   }
 }
-
-extension Update.UrlsEnvelope: Swift.Decodable {}
-
-extension Update.UrlsEnvelope.WebEnvelope: Swift.Decodable {}
