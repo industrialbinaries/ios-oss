5dff21782feddd7142205f7020275881e7966f86
Jan Timar <jantimar2@gmail.com>
2020-03-11 13:17:16 +0100

    Remove Argo dependency from Project

--- KsApi/models/Project.PhotoTests.swift
+++ KsApi/models/Project.PhotoTests.swift
@@ -3,40 +3,42 @@ import XCTest
 
 final class ProjectPhotoTests: XCTestCase {
   func testJSONParsing_WithPartialData() {
-    let photo = Project.Photo.decodeJSONDictionary([
+    let photo = Project.Photo.decodeJSON([
       "full": "http://www.kickstarter.com/full.jpg",
       "med": "http://www.kickstarter.com/med.jpg"
     ])
 
-    XCTAssertNotNil(photo.error)
+    guard case let .failure(error) = photo else {
+      XCTFail("Missing error value.")
+      return
+    }
+    XCTAssertNotNil(error)
   }
 
-  func testJSONParsing_WithMissing1024() {
-    let photo = Project.Photo.decodeJSONDictionary([
+  func testJSONParsing_WithMissing1024() throws {
+    let photo = try Project.Photo.decodeJSON([
       "full": "http://www.kickstarter.com/full.jpg",
       "med": "http://www.kickstarter.com/med.jpg",
       "small": "http://www.kickstarter.com/small.jpg"
-    ])
+      ]).get()
 
-    XCTAssertNil(photo.error)
-    XCTAssertEqual(photo.value?.full, "http://www.kickstarter.com/full.jpg")
-    XCTAssertEqual(photo.value?.med, "http://www.kickstarter.com/med.jpg")
-    XCTAssertEqual(photo.value?.small, "http://www.kickstarter.com/small.jpg")
-    XCTAssertNil(photo.value?.size1024x768)
+    XCTAssertEqual(photo.full, "http://www.kickstarter.com/full.jpg")
+    XCTAssertEqual(photo.med, "http://www.kickstarter.com/med.jpg")
+    XCTAssertEqual(photo.small, "http://www.kickstarter.com/small.jpg")
+    XCTAssertNil(photo.size1024x768)
   }
 
-  func testJSONParsing_WithFullData() {
-    let photo = Project.Photo.decodeJSONDictionary([
+  func testJSONParsing_WithFullData() throws {
+    let photo = try Project.Photo.decodeJSON([
       "full": "http://www.kickstarter.com/full.jpg",
       "med": "http://www.kickstarter.com/med.jpg",
       "small": "http://www.kickstarter.com/small.jpg",
       "1024x768": "http://www.kickstarter.com/1024x768.jpg"
-    ])
+    ]).get()
 
-    XCTAssertNil(photo.error)
-    XCTAssertEqual(photo.value?.full, "http://www.kickstarter.com/full.jpg")
-    XCTAssertEqual(photo.value?.med, "http://www.kickstarter.com/med.jpg")
-    XCTAssertEqual(photo.value?.small, "http://www.kickstarter.com/small.jpg")
-    XCTAssertEqual(photo.value?.size1024x768, "http://www.kickstarter.com/1024x768.jpg")
+    XCTAssertEqual(photo.full, "http://www.kickstarter.com/full.jpg")
+    XCTAssertEqual(photo.med, "http://www.kickstarter.com/med.jpg")
+    XCTAssertEqual(photo.small, "http://www.kickstarter.com/small.jpg")
+    XCTAssertEqual(photo.size1024x768, "http://www.kickstarter.com/1024x768.jpg")
   }
 }
