d95ba3cf4fb612ea68a54508153ab77e47e5ebf5
Jan Timar <jantimar2@gmail.com>
2020-03-12 17:07:25 +0100

    Remove Argo dependency from ChangePaymentMethodEnvelope

--- KsApi/models/ChangePaymentMethodEnvelope.swift
+++ KsApi/models/ChangePaymentMethodEnvelope.swift
@@ -1,29 +1,12 @@
-import Argo
-import Curry
-import Runes
+import Foundation
 
 public struct ChangePaymentMethodEnvelope {
   public let newCheckoutUrl: String?
   public let status: Int
 }
 
-extension ChangePaymentMethodEnvelope: Argo.Decodable {
-  public static func decode(_ json: JSON) -> Decoded<ChangePaymentMethodEnvelope> {
-    return curry(ChangePaymentMethodEnvelope.init)
-      <^> json <|? ["data", "new_checkout_url"]
-      <*> ((json <| "status" >>- stringToIntOrZero) <|> (json <| "status"))
-  }
-}
-
-private func stringToIntOrZero(_ string: String) -> Decoded<Int> {
-  return
-    Double(string).flatMap(Int.init).map(Decoded.success)
-    ?? Int(string).map(Decoded.success)
-    ?? .success(0)
-}
-
-extension ChangePaymentMethodEnvelope: Swift.Decodable {
-  private enum CodingKeys: String, CodingKey {
+extension ChangePaymentMethodEnvelope: Decodable {
+  enum CodingKeys: String, CodingKey {
     case data, status
   }
 
@@ -38,7 +21,7 @@ extension ChangePaymentMethodEnvelope: Swift.Decodable {
   }
 }
 
-private struct NewCheckoutURLData: Swift.Decodable {
+private struct NewCheckoutURLData: Decodable {
   private enum CodingKeys: String, CodingKey {
     case newCheckoutUrl = "new_checkout_url"
   }
