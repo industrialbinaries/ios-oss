c8c2b9ac5f0704b48dd8cc67e80c5b0d7a2a08d8
Jan Timar <jantimar2@gmail.com>
2020-03-12 13:45:00 +0100

    Extend Swift.Decodable with `decodeJSON`

--- KsApi/lib/Decodable.swift
+++ KsApi/lib/Decodable.swift
@@ -12,3 +12,30 @@ public extension Argo.Decodable {
     return Self.decode(JSON(json))
   }
 }
+
+public extension Swift.Decodable {
+  /**
+   Decode a JSON dictionary into a `Decoded` type.
+
+   - parameter json: A dictionary with string keys.
+
+   - returns: A decoded value.
+   */
+  static func decodeJSON(_ json: [AnyHashable: Any]) -> Result<Self, Error> {
+    do {
+      let data = try JSONSerialization.data(withJSONObject: json, options: .prettyPrinted)
+      return decode(data)
+    } catch {
+      return .failure(error)
+    }
+  }
+}
+
+struct Failable<T: Swift.Decodable>: Swift.Decodable {
+  let value: T?
+
+  init(from decoder: Decoder) throws {
+    let container = try decoder.singleValueContainer()
+    value = try? container.decode(T.self)
+  }
+}
