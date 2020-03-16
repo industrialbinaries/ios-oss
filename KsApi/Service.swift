3eff5430b01203c8722a3d12d10b16fd389fa5e6
Jan Timar <jantimar2@gmail.com>
2020-03-16 09:44:17 +0100

    Extend UpdatePledgeEnvelope with Swift.Encodable

--- KsApi/Service.swift
+++ KsApi/Service.swift
@@ -496,7 +496,7 @@ public struct Service: ServiceType {
     tappedReward: Bool
   ) -> SignalProducer<UpdatePledgeEnvelope, ErrorEnvelope> {
     return request(
-      .updatePledge(
+      route: .updatePledge(
         project: project,
         amount: amount,
         reward: reward,
